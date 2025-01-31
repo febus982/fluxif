f744e1b5d8c2935e5abf3be434fbefd2e20c8ecd
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-01-31 13:13:21 +0000

    Create local bootstrap helper

---
+++ local-boostrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#export GITHUB_TOKEN=op://Personal/FluxIF-GitHub-PAT/password
+kind create cluster --config=kind-cluster-config.yaml
+docker pull quay.io/cilium/cilium:v1.16.6
+kind load docker-image quay.io/cilium/cilium:v1.16.6
+
+./flux-bootstrap.sh
