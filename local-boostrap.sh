048e16d19c6f2ce49e2d3fd681015e3a85f87903
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-02-02 17:14:15 +0000

    Update bootstrap script

--- local-boostrap.sh
+++ local-boostrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-#export GITHUB_TOKEN=op://Personal/FluxIF-GitHub-PAT/password
+export GITHUB_TOKEN=op://Personal/FluxIF-GitHub-PAT/password
 kind create cluster --config=kind-cluster-config.yaml
 docker pull quay.io/cilium/cilium:v1.16.6
 kind load docker-image quay.io/cilium/cilium:v1.16.6
+cilium install -f ./kind-cilium-values.yaml
 
-./flux-bootstrap.sh
+op run -- ./flux-bootstrap.sh
