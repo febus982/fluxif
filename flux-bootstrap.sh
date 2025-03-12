ee9dbd869608f191fc7719148bbfa856c58a39ae
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-03-12 17:05:35 +0000

    Bit of refactor

--- flux-bootstrap.sh
+++ flux-bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#export GITHUB_TOKEN=op://Personal/FluxIF-GitHub-PAT/password
 flux bootstrap github \
   --token-auth \
   --owner=febus982 \
--- local-boostrap.sh
+++ local-boostrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 export GITHUB_TOKEN=op://Personal/FluxIF-GitHub-PAT/password
+export CILIUM_VERSION=v1.17.1
 kind create cluster --config=kind-cluster-config.yaml
-docker pull quay.io/cilium/cilium:v1.16.6
-kind load docker-image quay.io/cilium/cilium:v1.16.6
-cilium install -f ./kind-cilium-values.yaml
+docker pull quay.io/cilium/cilium:${CILIUM_VERSION}
+kind load docker-image quay.io/cilium/cilium:${CILIUM_VERSION}
+cilium install -f ./kind-cilium-values.yaml --version ${CILIUM_VERSION} --wait
 
 op run -- ./flux-bootstrap.sh
